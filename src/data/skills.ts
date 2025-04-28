4888c5c0229c933ef7e5523b3068bbf26b1eae4d
Brayan <brayanespitia10@gmail.com>
2025-04-28 14:07:07 -0500

    update front

---
+++ src/data/skills.ts
@@ -0,0 +1,17 @@
+export interface SkillData {
+  name: string;
+}
+
+export const skills: SkillData[] = [
+  { name: "Next.js" },
+  { name: "Nuxt.js" },
+  { name: "Node.js" },
+  { name: "Docker" },
+  { name: "SQL/noSQL" },
+  { name: "CI/CD" },
+  { name: "Cloud" },
+  { name: "Blockchain" },
+  { name: "n8n" },
+  { name: "Google Cloud Platform" },
+  { name: "Terraform" },
+];
